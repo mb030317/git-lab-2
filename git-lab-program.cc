24665b4f9552c03d47de6392c30e6290cf8b173c
Michael <mb030317@ohio.edu>
2020-09-02 16:59:10 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Brennen
+ *        Date: September 2nd, 2020
+ * Description: Program for gitlab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
